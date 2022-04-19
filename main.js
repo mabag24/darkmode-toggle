26887c68b6f34ad8da215243952eb547b45c844c
mabag24 <matthewbaggott@me.com>
2022-04-19 09:43:01 +0100

    cleanup and commenting

--- main.js
+++ main.js
@@ -8,33 +8,27 @@ window.onload = function () {
 
     let temp = JSON.parse(localStorage.getItem("state")); //read state
 
-    let savedPos = JSON.parse(localStorage.getItem("icon-pos"));
+    let savedPos = JSON.parse(localStorage.getItem("icon-pos")); // reads where the sun/moon should be
 
-    // check what is saved. If its dark then toggle both, if its light toggle nothign??
+    // check what is saved. If the state is saved to dark, toggle both light and dark, otherwise do nothing
 
     if (temp == "dark") {
         document.body.classList.toggle("dark");
         document.body.classList.toggle("light");
-        btn.classList.toggle("dark");
-        btn.classList.toggle("light");
-
     }
 
     sunMoonContainer.style.setProperty("--rotation", savedPos); //saves position of sun/moon icon
 }
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", () => { // when the button is clicked, toggle the theme
     document.body.classList.toggle("dark");
     document.body.classList.toggle("light");
 
-    btn.classList.toggle("dark");
-    btn.classList.toggle("light");
-
     let currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue("--rotation")); //get the value of the css variable "--rotation"
 
-    sunMoonContainer.style.setProperty("--rotation", currentRotation + 180); //adds 180 to the variable
+    sunMoonContainer.style.setProperty("--rotation", currentRotation + 180); //adds 180 to the variable to show the sun instead of the moon or vice versa
 
-    save(document.body.classList[0]);
+    save(document.body.classList[0]); // saves the current theme by getting the first value in element.classList
 });
 
 save = (value) => {
@@ -46,7 +40,7 @@ save = (value) => {
     localStorage.setItem("state", JSON.stringify(value)); //saves to localstorage maybe??
 
 
-    localStorage.setItem("icon-pos", rotateReset(parseInt(getComputedStyle(sunMoonContainer).getPropertyValue("--rotation"))));
+    localStorage.setItem("icon-pos", rotateReset(parseInt(getComputedStyle(sunMoonContainer).getPropertyValue("--rotation")))); // saves the rotation, so that the icon can be lined up correctly when the page loads
 }
 
 rotateReset = (angle) => {
